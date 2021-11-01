ec48b4cb35261daef14dfaa691eed89294cf34ff
tonyprr <tonyprr@gmail.com>
2021-11-01 00:01:59 -0500

    first commit

---
+++ src/main/java/com/tonyprr/demos/kafka/ProducerDemo.java
@@ -0,0 +1,38 @@
+package com.tonyprr.demos.kafka;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
+public class ProducerDemo {
+
+    public static void main(String[] args) {
+
+        String bootstrapServers = "127.0.0.1:9092";
+
+        // Create Producer properties
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        // create the Producer
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
+
+        // create a producer record
+        ProducerRecord<String, String> record =
+                new ProducerRecord<>("first_topic", "Hola mundo!!");
+
+        // send data - asynchronous
+        producer.send(record);
+
+        // flush data
+        producer.flush();
+
+        // flush close and close producer
+        producer.close();
+    }
+}
---
+++ src/main/java/com/tonyprr/demos/kafka/ProducerDemoKeys.java
@@ -0,0 +1,63 @@
+package com.tonyprr.demos.kafka;
+
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+
+public class ProducerDemoKeys {
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+
+        Logger logger = LoggerFactory.getLogger(ProducerDemoKeys.class);
+
+        String bootstrapServers = "127.0.0.1:9092";
+
+        // Create Producer properties
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        // create the Producer
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
+
+        for (int i = 0; i < 10; i++) {
+            // create a producer record
+            String topic = "first_topic";
+            String value = "Hello World " + i;
+            String key = "id_" + i;
+
+            ProducerRecord<String, String> record =
+                    new ProducerRecord<>(topic, key, value);
+
+            logger.info("Key: " + key);
+            // send data - asynchronous
+            producer.send(record, new Callback() {
+                @Override
+                public void onCompletion(RecordMetadata recordMetadata, Exception e) {
+                    // execute every time a record is successfully sent or an exception is thrown
+                    if (e == null) {
+                        // the record was successfully sent
+                        logger.info("Received new metadata: \n" +
+                                "Topic: " + recordMetadata.topic() + "\n" +
+                                "Partition: " + recordMetadata.partition() + "\n" +
+                                "Offset: " + recordMetadata.offset() + "\n" +
+                                "Timestamp: " + recordMetadata.timestamp());
+                    } else {
+                        logger.error("Error while producing", e);
+                    }
+                }
+            }).get(); // block the .send to make it synchronous - don't do this in production!
+        }
+
+        // flush data
+        producer.flush();
+
+        // flush close and close producer
+        producer.close();
+    }
+}
